7804bd7fd878776591948e54434b9502bbc7d8da
Jaxxi <te14.linus.olofsson@umea.nti.se>
2016-09-02 11:20:01 +0200

    test2

---
+++ reverse.java
@@ -0,0 +1,26 @@
+class reverse {
+    static void reversei(int arr[], int start, int end)
+    {
+        int temp;
+        if (start >= end)
+            return;
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reversei(arr, start+1, end-1);
+    }
+    static void printArray(int arr[], int size)
+    {
+        int i;
+        for (i=0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+    public static void main (String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        printArray(arr, 6);
+        rvereseArray(arr, 0, 5);
+        System.out.println("Reversed array");
+        printArray(arr, 6);
+    }
+}
